5c6108d3eb7bd41d616e40ad20de557346d32ebd
Adrien Le Verger <adrienleverger@gmail.com>
2026-06-22 18:14:54 +0200

    Modif pour faire fonctionner les modules npm, Ajout de page, Fix divers

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const configPath = (window.location.pathname.includes('/Formations/') || window.location.pathname.includes('/Competences/')) ? '../assets/config/particles.json' : 'assets/config/particles.json';
+const configPath = (window.location.pathname.includes('/Formations/') || window.location.pathname.includes('/Competences/') || window.location.pathname.includes('/Expériences/')) ? '../assets/config/particles.json' : 'assets/config/particles.json';
 particlesJS.load('particles-js', configPath, function() {
     console.log('INFO - particles.js config loaded');
 });
